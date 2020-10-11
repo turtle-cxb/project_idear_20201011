8f7bd4c39a7c3fa9753fff2541814ba4d50b2843
turtle <turtle@qq.com>
2020-10-11 14:06:03 +0800

    Hello World hot-fix first commit

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -3,5 +3,6 @@ public class HelloWord {
         System.out.println("Hello,World");
         System.out.println("陈小柏是个逗比");
         System.out.println("陈小柏是个傻蛋");
+        System.out.println("陈小柏是个二逼");
     }
 }
